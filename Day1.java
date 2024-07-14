0c4118676ea8931b6984496ec67e3881ee6870d1
adi22ty <22adityathakur@gmail.com>
2024-07-14 12:54:23 +0530

    First push

---
+++ Day1.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class day1 {
+    public static void main(String args[]) {
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i);
+        // }
+        // int i=1;
+        // Scanner sc =new Scanner(System.in);
+        // while(i<=10) {
+        //     System.out.println(i);
+        //     i++;
+
+        // }
+        // Sc.close();
+        // Take an integer n as input and print a * n times
+        
+        // Scanner sc = new Scanner (System.in);
+        // int n=sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     System.out.print("* ");
+
+        // }
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 System.out.print("* ");
+//             }
+
+//  System.out.println();
+// 
+// int n=4;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print("* ");
+//     }
+//     System.out.println();
+// }     
+// int n=4;
+// for(int i=1;i<=4;i++){
+//     for(int k=1;k<=4;k++){
+//         System.out.print("  ");
+
+//     }
+//     for(int j=1; j<=i;j++){
+//         System.out.print("* ");
+//     }
+//     System.out.println();
+   
+// }
+// int n=4;
+// for(int i=n; i>=1;i--){
+//     for(int j=1; j<=i;j++){
+//         System.out.print("* ");
+//     }
+//     System.out.println(" ");
+// }
+// int n=4;
+// for(int i=n;i>=1;i--){
+//     for(int k=1;k<=4-i;k++){
+//         System.out.print("    ");
+
+//     }
+//     for(int j=1; j<=i;j++){
+//         System.out.print("* ");
+//     }
+//     System.out.println();
+   
+// }
+int n=5;
+for(int i=1;i<=5;i++){
+    for(int j=1; j<=n; j++){
+        if(i==5 || i==1 || j==1 || j==5){
+            System.out.print("* ");
+        }
+        else{
+            System.out.print("  ");
+        }
+        
+
+}
+System.out.println();
+    }
+
+
+
+    
+    }
+
+}
